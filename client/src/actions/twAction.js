179e41538c0d7d073f546f7cd0e92d7642201622
zzb <694344795@qq.com>
2017-04-12 22:12:54 +0800

    "new-zzb"

--- client/src/actions/twAction.js
+++ client/src/actions/twAction.js
@@ -1,30 +1,19 @@
-var {CHANGE,DELETE,ADD,SHOW}  = require('../constants/twConstant')
+var {CHANGE,DELETE,ADD}  = require('../constants/twConstant')
 
 
 exports.add = function(){
-
     return{
         type:ADD
     };
     
 }
-exports.show = function(){
-
-    return{
-        type:SHOW
-    };
-    
-}
 exports.del = function(){
-    
    return{
-        type:DELETE,
-
+        type:DELETE
     };
     
 }
 exports.change = function(){
-    
     return{
         type:CHANGE
     };
--- client/src/constants/twConstant.js
+++ client/src/constants/twConstant.js
@@ -1,4 +1,3 @@
-exports.ADD= 'ADD';
+
 exports.CHANGE= 'CHANGE';
 exports.DELETE= 'DELETE';
-exports.SHOW= 'SHOW';
